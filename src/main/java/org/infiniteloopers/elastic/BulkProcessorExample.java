420bb3f6d186304ff3739c85ab77ef9d9c5f1041
Selvakumar Esra <selvakumar.esra@gmail.com>
2015-12-10 01:49:18 +0800

    Initial commit

---
+++ src/main/java/org/infiniteloopers/elastic/BulkProcessorExample.java
@@ -0,0 +1,156 @@
+package org.infiniteloopers.elastic;
+
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.io.IOException;
+
+public class BulkProcessorExample {
+    public static void bulkIndexBySize(Client client) {
+        String index = "test";
+
+        //delete index if exists
+        if (client.admin().indices().prepareExists(index).execute().actionGet().isExists())
+            client.admin().indices().prepareDelete(index).execute().actionGet();
+
+        final BulkProcessor bp = BulkProcessor.builder(client, new BulkProcessor.Listener() {
+            @Override
+            public void beforeBulk(long executionId, BulkRequest request) {
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                System.out.println("Bulk execution failed [" + executionId + "].\n" +
+                        failure.toString());
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                System.out.println("Bulk execution completed [" + executionId + "].\n" +
+                        "Took (ms): " + response.getTookInMillis() + "\n" +
+                        "Failures: " + response.hasFailures() + "\n" +
+                        "Count: " + response.getItems().length);
+            }
+        })
+                .setConcurrentRequests(4)
+                .setBulkActions(-1)
+                .setBulkSize(new ByteSizeValue(1, ByteSizeUnit.MB))
+                .build();
+
+        try {
+            Settings settings = Settings.settingsBuilder()
+                    .put("number_of_shards", 1)
+                    .put("number_of_replicas", 0)
+                    .build();
+
+            client.admin().indices().prepareCreate(index).setSettings(settings).execute().actionGet();
+
+            client.admin().cluster().prepareHealth(index).setWaitForYellowStatus().execute().actionGet();
+
+            for (int i = 0; i < 10000; i++) {
+                try {
+                    XContentBuilder source = XContentFactory.jsonBuilder().startObject()
+                            .field("test", "this is document " + i)
+                            .endObject();
+
+                    bp.add(Requests.indexRequest(index).type("doc").source(source));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            settings = Settings.settingsBuilder()
+                    .put("refresh_interval", "1s")
+                    .build();
+
+            client.admin().indices().prepareUpdateSettings(index).setSettings(settings).execute().actionGet();
+        } finally {
+            bp.close();
+        }
+    }
+
+    public static void bulkIndexByActions(Client client) {
+        String index = "test";
+
+        //delete index if exists
+        if (client.admin().indices().prepareExists(index).execute().actionGet().isExists())
+            client.admin().indices().prepareDelete(index).execute().actionGet();
+
+        final BulkProcessor bp = BulkProcessor.builder(client, new BulkProcessor.Listener() {
+            @Override
+            public void beforeBulk(long executionId, BulkRequest request) {
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                System.out.println("Bulk execution failed [" + executionId + "].\n" +
+                        failure.toString());
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                System.out.println("Bulk execution completed [" + executionId + "].\n" +
+                        "Took (ms): " + response.getTookInMillis() + "\n" +
+                        "Failures: " + response.hasFailures() + "\n" +
+                        "Count: " + response.getItems().length);
+            }
+        })
+                .setConcurrentRequests(4)
+                .setBulkActions(1000)
+                .setBulkSize(new ByteSizeValue(-1))
+                .build();
+
+        try {
+            Settings settings = Settings.settingsBuilder()
+                    .put("number_of_shards", 1)
+                    .put("number_of_replicas", 0)
+                    .build();
+
+            ThreadPool threadPool = client.admin().cluster().threadPool();
+            threadPool.updateSettings(Settings.settingsBuilder().put("threadpool.bulk.queue_size", "1000").build());
+            threadPool.updateSettings(Settings.settingsBuilder().put("threadpool.bulk.size", "30").build());
+
+
+            threadPool.updateSettings(Settings.settingsBuilder().put("threadpool.index.queue_size", "1000").build());
+            threadPool.updateSettings(Settings.settingsBuilder().put("threadpool.index.size", "30").build());
+
+
+            client.admin().indices().prepareUpdateSettings(index).setSettings(Settings.settingsBuilder().put("index.translog.flush_threshold_size", "1GB").build()).execute().actionGet();
+
+
+            client.admin().indices().prepareCreate(index).setSettings(settings).execute().actionGet();
+
+            client.admin().cluster().prepareHealth(index).setWaitForYellowStatus().execute().actionGet();
+
+            for (int i = 0; i < 10000; i++) {
+                try {
+                    XContentBuilder source = XContentFactory.jsonBuilder().startObject()
+                            .field("test", "this is document " + i)
+                            .endObject();
+
+                    bp.add(Requests.indexRequest(index).type("doc").source(source));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            settings = Settings.settingsBuilder()
+                    .put("refresh_interval", 1)
+                    .build();
+
+
+            client.admin().indices().prepareUpdateSettings(index).setSettings(settings).execute().actionGet();
+        } finally {
+            bp.close();
+        }
+    }
+}
